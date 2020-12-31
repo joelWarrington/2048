45e6d832dc8ff2728aedc413e3e57c03a7865952
joelWarrington <joelwarrington@gmail.com>
2020-12-31 01:10:25 -0700

    completed test cases

--- src/GameBoard.test.ts
+++ src/GameBoard.test.ts
@@ -1,4 +1,4 @@
-import GameBoardAPI, { MoveDirection } from './GameBoard';
+import GameBoardAPI, { MoveDirection, GameStatus } from './GameBoard';
 
 describe('BoardGame API', () => {
   test('Board Initialization', () => {
@@ -18,13 +18,12 @@ describe('BoardGame API', () => {
   });
 
   test('Tile Movement', () => {
-    const gameboard = new GameBoardAPI();
-    gameboard.board = [
+    const gameboard = new GameBoardAPI([
       [2, 2, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 4, 0],
       [0, 0, 4, 0],
-    ];
+    ]);
     gameboard.moveTiles(MoveDirection.Down);
     expect(gameboard.board).toStrictEqual([
       [0, 0, 0, 0],
@@ -57,9 +56,48 @@ describe('BoardGame API', () => {
 
   test('Available Move Condition', () => {
     const gameboard = new GameBoardAPI();
+    gameboard.initializeBoard();
+    expect(gameboard.getBoardStatus()).toBe(GameStatus.InProgress);
+    gameboard.board = [
+      [4, 16, 4, 8],
+      [16, 32, 16, 8],
+      [16, 32, 16, 4],
+      [2, 4, 4, 4],
+    ];
+    expect(gameboard.getBoardStatus()).toBe(GameStatus.InProgress);
   });
 
-  test('Win Condition', () => {});
+  test('Win Condition', () => {
+    const gameboard = new GameBoardAPI([
+      [2048, 16, 4, 8],
+      [16, 32, 16, 8],
+      [16, 32, 16, 4],
+      [2, 0, 4, 4],
+    ]);
+    expect(gameboard.getBoardStatus()).toBe(GameStatus.Won);
+    gameboard.board = [
+      [2, 16, 4, 8],
+      [16, 32, 16, 8],
+      [16, 32, 16, 4],
+      [2, 0, 4, 2048],
+    ];
+    expect(gameboard.getBoardStatus()).toBe(GameStatus.Won);
+  });
 
-  test('Loss Condition', () => {});
+  test('Loss Condition', () => {
+    const gameboard = new GameBoardAPI([
+      [32, 16, 2, 4],
+      [16, 4, 8, 16],
+      [4, 32, 16, 4],
+      [2, 64, 4, 2],
+    ]);
+    expect(gameboard.getBoardStatus()).toBe(GameStatus.Lost);
+    gameboard.board = [
+      [4, 2, 4, 2],
+      [8, 16, 8, 16],
+      [32, 64, 32, 64],
+      [128, 256, 128, 256],
+    ];
+    expect(gameboard.getBoardStatus()).toBe(GameStatus.Lost);
+  });
 });
